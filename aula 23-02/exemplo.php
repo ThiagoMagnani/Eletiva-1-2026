828008fdac88a54da6304c5ed84f396a780c98eb
Michael <michael.vicente1705@gmail.com>
2026-02-23 20:26:48 -0300

    Inicio PHP

---
+++ aula 23-02/exemplo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="" href="stylesheet">
+</head>
+<body>
+    <div class="container py-3">
+        <h1>Olá</h1>
+        <h3>Data de hoje: <?php echo date("d/m/Y"); ?></h3>
+        <form method="post" action="resposta.php">
+            <div class="mb-3">
+                <label for="nome" class="from-label">Insira seu nome:</label>
+                
+            </div>
+        </form>
+    </div>
+</body>
+</html>
