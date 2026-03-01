72cf200056595436a48576f6a6ae5ceca488dc4b
Thiago Arques <thiagoarques2@gmail.com>
2026-03-01 17:16:57 -0300

    exercicio11

---
+++ aula 24-02/exercicio13.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1></h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="num1" class="form-label">Insira a largura do retângulo</label>
+                <input type="text" id="num1" name="num1" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="num2" class="form-label">Inserir o a altura do retângulo</label>
+                <input type="text" id="num2" name="num2" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $valor1 = $_POST['num1'];
+            $valor2 = $_POST['num2'];
+            $perimetro = 2 * ($valor1 + $valor2);
+            echo "O perímetro do retângulo é: $perimetro";
+        }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
