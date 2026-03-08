a54b07aa8ec77c4c11e67225bf0e36460990d463
Thiago Arques <thiagoarques2@gmail.com>
2026-03-08 19:19:43 -0300

    atv3 - ex1

---
+++ Atividade 3/exercicio1.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1></h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="num1" class="form-label">Insira 7 números separados por espaço: </label>
+                <input type="text" name="numeros" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+            if($_SERVER["REQUEST_METHOD"]=="POST"){
+                $entrada = $_POST["numeros"];
+                $numeros = explode(" ", $entrada);
+
+                $menor = $numeros[0];
+                $posicao = 0;
+
+                for($i=0; $i<7; $i++){
+                    if($numeros[$i] < $menor){
+                        $menor = $numeros[$i];
+                        $posicao = $i;
+                    }
+                }
+
+                echo "Menor número: $menor <br>";
+                echo "Posição: $posicao";
+            }   
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+            crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
