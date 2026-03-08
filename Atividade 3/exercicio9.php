8c79472b4910ba299b3da2b22955d1ec77149f8f
Thiago Arques <thiagoarques2@gmail.com>
2026-03-08 19:24:23 -0300

    atv3 - ex3

---
+++ Atividade 3/exercicio9.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1></h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="num1" class="form-label">Insira o primeiro número: </label>
+                <input type="text" name="a" class="form-control" required="">
+            </div>
+            <div class="mb-3">
+                <label for="num2" class="form-label">Insira o segundo número: </label>
+                <input type="text" name="b" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $a = $_POST["a"];
+            $b = $_POST["b"];
+
+            $soma = $a + $b;
+
+            if ($a == $b) {
+                $resultado = $soma * 3;
+            } else {
+                $resultado = $soma;
+            }
+
+            echo "Resultado: $resultado";
+        }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+            crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
