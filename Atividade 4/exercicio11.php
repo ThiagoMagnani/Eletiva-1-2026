38b180561a1581960d88f0b0a16dd96f3d24ac35
Thiago Arques <thiagoarques2@gmail.com>
2026-03-14 21:28:23 -0300

    atv4 - ex10

---
+++ Atividade 4/exercicio11.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container py-3">
+        <h1></h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="frase" class="form-label">Insira a frase: </label>
+                <input type="text" name="frase" class="form-control" required="">
+            </div>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $frase = $_POST["frase"];
+
+            $fraseSemEspaco = str_replace(" ", "", $frase);
+            $fraseSemEspacoComFin = trim($frase);
+
+            echo "<p>Frase original: $frase";
+            echo "<p>Frase sem espaço: $fraseSemEspaco";
+            echo "<p>Frase sem espaço no começo e final: $fraseSemEspacoComFin";
+
+        }
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO"
+            crossorigin="anonymous"></script>
+    </div>
+</body>
+
+</html>
