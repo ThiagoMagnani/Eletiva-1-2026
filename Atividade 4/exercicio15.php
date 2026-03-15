09c45770b977447c844dcf2f40cde23691ff3fb9
Thiago Arques <thiagoarques2@gmail.com>
2026-03-14 22:38:18 -0300

    atv4 - ex15

--- Atividade 4/exercicio15.php
+++ Atividade 4/exercicio15.php
@@ -13,16 +13,17 @@
         <h1></h1>
         <form method="post">
             <div class="mb-3">
-                <label for="valor" class="form-label">Insira um valor: </label>
-                <input type="text" name="valor" class="form-control" required="">
+                <label for="email" class="form-label">Insira um email: </label>
+                <input type="email" name="email" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $valor = $_POST["valor"];
+            $email = $_POST["email"];
 
-            echo 'R$ ' . number_format($valor, 2, ',', '.');
+            $dominio = substr(strrchr($email, "@"), 1);
+            echo "<p>O domínio do email é: $dominio</p>";
         }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
