ce766f9d68e73d26a028b3a039409780d44e4d9c
Thiago Arques <thiagoarques2@gmail.com>
2026-03-14 22:32:22 -0300

    atv4 - ex13

--- Atividade 4/exercicio13.php
+++ Atividade 4/exercicio13.php
@@ -13,16 +13,25 @@
         <h1></h1>
         <form method="post">
             <div class="mb-3">
-                <label for="valor" class="form-label">Insira um valor: </label>
-                <input type="text" name="valor" class="form-control" required="">
+                <label for="frase" class="form-label">Insira uma frase: </label>
+                <input type="text" name="frase" class="form-control" required="">
             </div>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $valor = $_POST["valor"];
+            $frase = $_POST["frase"];
 
-            echo 'R$ ' . number_format($valor, 2, ',', '.');
+            $ContagemLetras = strlen($frase);
+            $MaiorPalavra = 0;
+            $palavras = explode(" ", $frase);
+            foreach ($palavras as $palavra) {
+                if (strlen($palavra) > strlen($MaiorPalavra)) {
+                    $MaiorPalavra = $palavra;
+                }
+            }
+            echo "A frase tem $ContagemLetras letras.";
+            echo "<br>Maior palavra: $MaiorPalavra";
         }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
