22514e53078b17e871c4b84e44809dbca0701967
Thiago Arques <thiagoarques2@gmail.com>
2026-05-05 21:47:54 -0300

    projeto

--- projeto/alterar_categoria.php
+++ projeto/alterar_categoria.php
@@ -1,6 +1,23 @@
 <?php
     require_once('cabecalho.php');
     require_once('conexao.php');
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $nome = $_POST['descricao'];
+        $id = $_GET['id'];
+        try{
+            $sql = "UPDATE categoria SET nome = ? WHERE id = ?";
+          $stmt = $pdo->prepare($sql);
+          if($stmt->execute([$nome, $id])){
+            $mensagem = "<p>Alteração realizada!</p>";
+          } else {
+            $mensagem = "<p>Erro ao alterar! Tente novamente</p>";
+          }
+        } catch(Exception $e){
+          echo "Erro: ".$e->getMessage();
+        }
+      }
+
     try{
         $stmt = $pdo->prepare("SELECT * FROM categoria WHERE id = ?");
         $stmt->execute([$_GET['id']]);
@@ -15,29 +32,14 @@
         <div class="mb-3">
               <label for="descricao" class="form-label">Informe a descrição</label>
               <input value="<?= $resultado['nome']?>" type="text" id="descricao" name="descricao" class="form-control" required="">
-              <input value="<?= $resultado['nome']?>" type="text">
         </div>
         <button type="submit" class="btn btn-primary">Enviar</button>
     </form>
 
     <?php
-      if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        require_once('conexao.php');
-        $nome = $_POST['descricao'];
-        $id = $_GET['id'];
-        try{
-            $sql = "UPDATE categoria SET nome = ? WHERE id = ?";
-          $stmt = $pdo->prepare($sql);
-          if($stmt->execute([$nome, $id])){
-            echo "<p>Alteração realizada!</p>";
-          } else {
-            echo "<p>Erro ao alterar! Tente novamente</p>";
-          }
-        } catch(Exception $e){
-          echo "Erro: ".$e->getMessage();
-        }
-      }
+      echo $mensagem;
     ?>
 
 <?php
     require_once('rodape.php');
+?>
